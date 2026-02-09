21f8663f01f833db4486cfd318ede89279e89c16
kiril.marinov <kiril.marinov@siemens.com>
2026-02-09 16:06:28 +0200

    Add User Password and Authentication

---
+++ 10-20260204/controllers/authController.js
@@ -0,0 +1,29 @@
+const usersDB = {
+  users: require("../model/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+
+const bcrypt = require("bcrypt");
+
+const handleLogin = async (req, res) => {
+  const { user, password } = req.body;
+  if (!user || !password) {
+    return res.status(400).json({ message: "User and password missing" });
+  }
+
+  const foundUser = usersDB.users.find((person) => person.username === user);
+  if (!foundUser) {
+    return res.status(401); // 401 - unauthorized
+  }
+
+  const match = await bcrypt.compare(password, foundUser.password);
+  if (match) {
+    res.json({ success: `User ${user} is logged in.` });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+module.exports = { handleLogin };
---
+++ 10-20260204/controllers/registerController.js
@@ -0,0 +1,39 @@
+const usersDB = {
+  users: require("../model/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+
+const fsPromises = require("fs").promises;
+const path = require("path");
+const bcrypt = require("bcrypt");
+
+const handleNewUser = async (req, res) => {
+  const { user, password } = req.body;
+  if (!user || !password) {
+    return res.status(400).json({ message: "User and password missing" });
+  }
+
+  const dublicate = usersDB.users.find((person) => person.username === user);
+  if (dublicate) {
+    return res.status(409); // 409 - conflict
+  }
+  try {
+    // encrypt the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // store new user
+    const newUser = { username: user, password: hashedPassword };
+    usersDB.setUsers([...usersDB.users, newUser]);
+    await fsPromises.writeFile(
+      path.join(__dirname, "..", "model", "users.json"),
+      JSON.stringify(usersDB.users),
+    );
+    console.log(usersDB.users);
+    res.status(201).json({ success: "New user created" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { handleNewUser };
